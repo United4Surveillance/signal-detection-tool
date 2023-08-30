ab59a15f1a3efe68abfa378f518e3e533d337cf8
Aapo Juutinen <aapo.juutinen@thl.fi>
2023-08-30 15:00:35 +0300

    Added new folders (R & Outputs) and few scripts  to outline repository structure.

---
+++ R/main.R
@@ -0,0 +1,3 @@
+# This file should include the main R script of the package
+# All other necessary code files, data etc. should be sourced in this script
+# Feel free to complete the file
---
+++ R/tool_functions.R
@@ -0,0 +1,2 @@
+# This file should include all relevant helper functions required to run the actual signal detection tool
+# Feel free to complete the file
