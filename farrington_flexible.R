24480140b237aedfebc637ecb23ea863a936d739
Alexander Ullrich <UllrichA@rki.de>
2023-09-11 17:18:32 +0200

    adds new r script for farrington flexible, adds examples for the outbrea data formats

--- farrington_flexible.R
+++ farrington_flexible.R
@@ -27,6 +27,27 @@ convert_to_sts <- function(data) {
   ))
 }
 
+preprocess_data <- function(data){
+  
+  # Convert the date columns to date format
+  data <- data %>%
+    dplyr::mutate(across(starts_with("date"), ~ as.Date(.x,optional=T)))
+  
+  # add columns for isoyear and isoweek for each date
+  data <- data %>%
+    dplyr::mutate(across(starts_with("date")&!where(is.numeric), ~ surveillance::isoWeekYear(.x)$ISOYear, .names = "{.col}_year")) %>%
+    dplyr::mutate(across(starts_with("date")&!where(is.numeric), ~ surveillance::isoWeekYear(.x)$ISOWeek, .names = "{.col}_week"))
+  
+  data
+}
+
+aggregate_data <- function(data){
+  
+  data %>%
+    dplyr::group_by(date_onset_year, date_onset_week) %>%
+    dplyr::summarize(cases = n())
+}
+
 get_signals_farringtonflexible <- function(surveillance_data){
   sts_cases <- convert_to_sts(data)
   
