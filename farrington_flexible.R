374707ae00ca14a71708ff732911cf7c66b025b2
Alexander Ullrich <UllrichA@rki.de>
2023-09-11 21:32:39 +0200

    adds methods for preprocessing and aggregating

--- farrington_flexible.R
+++ farrington_flexible.R
@@ -2,26 +2,13 @@ library(surveillance)
 library(tidyverse)
 
 # turn data into sts object
-convert_to_sts <- function(data) {
-  # Convert the 'date_onset' column to a date format
-  data <- data %>%
-    dplyr::mutate(date_onset = as.Date(date_onset)) %>%
-    dplyr::arrange(date_onset)
-  
-  year_week <- surveillance::isoWeekYear(data$date_onset)
-  
-  # aggregate case counts per week
-  case_counts <- data %>%
-    dplyr::mutate(isoyear = year_week$ISOYear) %>%
-    dplyr::mutate(isoweek = year_week$ISOWeek) %>%
-    dplyr::group_by(isoyear, isoweek) %>%
-    dplyr::summarize(cases = n())
+convert_to_sts <- function(case_counts) {
   
   # create sts object
   return(surveillance::sts(case_counts$cases,
                            start = c(
-                             case_counts$isoyear[1],
-                             case_counts$isoweek[1]
+                             case_counts$year[1],
+                             case_counts$week[1]
                            ),
                            frequency = 52
   ))
@@ -45,14 +32,39 @@ aggregate_data <- function(data){
   
   data %>%
     dplyr::group_by(date_onset_year, date_onset_week) %>%
-    dplyr::summarize(cases = n())
+    dplyr::summarize(cases = n()) %>%
+    dplyr::select(year = date_onset_year, week = date_onset_week, cases)
 }
 
 get_signals_farringtonflexible <- function(surveillance_data){
+  
+  data <- preprocess_data(surveillance_data) %>% aggregate_data()
+  
   sts_cases <- convert_to_sts(data)
   
-  # run Farrington Flexible on data
-  results <- surveillance::farringtonFlexible(sts_cases)
+  num_weeks <- length(sts_cases@observed)
+  num_years <- floor((num_weeks - 26)/52)-1
   
-  return(results)
+  control  <- list(range=((num_weeks-51):num_weeks),
+                   noPeriods=10,populationOffset=FALSE,
+                   fitFun="algo.farrington.fitGLM.flexible",
+                   b=num_years,w=3,weightsThreshold=2.58,
+                   pastWeeksNotIncluded=26,
+                   pThresholdTrend=1,trend=TRUE,
+                   thresholdMethod="delta",alpha=0.1)
+  
+  
+  # run Farrington Flexible on data
+  results <- surveillance::farringtonFlexible(sts_cases, control)
+  
+  pad <- rep(NA,num_weeks-52)
+  alarms <- c(pad,results@alarm)
+  upperbound <- c(pad, results@upperbound)
+  expected <- c(pad, results@control$expected)
+  
+  data$alarms <- alarms
+  data$upperbound <- upperbound
+  data$expected <- expected
+  
+  return(data)
 }
--- start.R
+++ start.R
@@ -1,39 +1,12 @@
 library(surveillance)
 library(tidyverse)
-
-# turn data into sts object
-convert_to_sts <- function(data) {
-  # Convert the 'date_onset' column to a date format
-  data <- data %>%
-    dplyr::mutate(date_onset = as.Date(date_onset)) %>%
-    dplyr::arrange(date_onset)
-
-  year_week <- surveillance::isoWeekYear(data$date_onset)
-
-  # aggregate case counts per week
-  case_counts <- data %>%
-    dplyr::mutate(isoyear = year_week$ISOYear) %>%
-    dplyr::mutate(isoweek = year_week$ISOWeek) %>%
-    dplyr::group_by(isoyear, isoweek) %>%
-    dplyr::summarize(cases = n())
-
-  # create sts object
-  return(surveillance::sts(case_counts$cases,
-    start = c(
-      case_counts$isoyear[1],
-      case_counts$isoweek[1]
-    ),
-    frequency = 52
-  ))
-}
+source("farrington_flexible.R")
 
 # load example data
 input_path <- "data/input/input.csv"
 data <- read.csv(input_path, header = TRUE, sep = ",")
 
-sts_cases <- convert_to_sts(data)
-
 # run Farrington Flexible on data
-results <- surveillance::farringtonFlexible(sts_cases)
+results <- get_signals_farringtonflexible(data)
 
 print(results)
