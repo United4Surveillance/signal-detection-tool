824cad423432ac926385b85eee41e2d3b788816b
alchalu <alena.chalupka@ages.at>
2023-10-19 15:55:29 +0200

    update: export age_groups

--- R/utils.R
+++ R/utils.R
@@ -16,13 +16,5 @@ NULL
 #' @importFrom rlang .data
 NULL
 
-#' Copy of guide_colourbar_interactive
-#'
-#' @description
-#' fixes weird problem in vignette since match.fun only looks back through 2
-#' layers of parent environments for the named function
-#'
-#' @keywords internal
-#' @noRd
 #' @importFrom ggiraph guide_colourbar_interactive
 NULL
