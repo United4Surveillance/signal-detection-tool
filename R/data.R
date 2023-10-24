8fb14134829368373391d7c1746d6d473552e9ae
Handra Norbert <norbert.handra@ages.at>
2023-10-24 11:47:19 +0200

    export metadata markdown table as a data frame called input_metadata

--- R/data.R
+++ R/data.R
@@ -24,3 +24,17 @@
 #' }
 #' @source TODO
 "nuts_shp"
+
+
+#' Input metadata
+#'
+#' Data containing the metadata of input files
+#'
+#' @format ## `input_metadata`
+#' A data frame with 29 rows and 3 columns:
+#' \describe{
+#'   \item{TO}{DO}
+#'   ...
+#' }
+#' @source TODO
+"input_metadata"
