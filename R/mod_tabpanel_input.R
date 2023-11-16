c7223def5f48415fe4b7dc5273da42340659b7b4
jaemol <jmoller72@gmail.com>
2023-11-16 10:55:02 +0100

    trying to optimize data use

--- R/mod_tabpanel_input.R
+++ R/mod_tabpanel_input.R
@@ -2,13 +2,19 @@ mod_tabpanel_input_ui <- function(id) {
   ns <- shiny::NS(id)
 
   shiny::tabPanel(
-    "Input parameters",
+    "Input parameters"
 
-    h2("Choose which pathogen in the dataset to check for aberrations"),
-    br(),
-    #selectInput("pathogen", "Choose pathogen", c("-", "covid-19", "E-coli", "Salmonella", "Pertussis"), selected = "-"),
-    uiOutput(NS(id,"path_checkbox")),
-    icon = icon("viruses")
+    ,h2("Choose which pathogen in the dataset to check for aberrations")
+    ,br()
+
+    ,shiny::uiOutput(NS(id,"pathogen_choices"))
+
+    ,h2("Choose stratification parameters")
+    ,br()
+
+    ,shiny::uiOutput(NS(id,"strat_choices"))
+
+    ,icon = icon("viruses")
   )
 }
 
@@ -17,19 +23,51 @@ mod_tabpanel_input_server <- function(id) {
   shiny::moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    output$path_checkbox <- renderUI({
-      df <- read.csv(input$file1$datapath,
-                     header = input$header,
-                     sep = input$sep,
-                     quote = input$quote)
+    ## showing options in ui
+    output$pathogen_choices <- shiny::renderUI({
 
-      if (input$dates_bin) {
-        df <- subset(df, between(as.Date(date_report), input$min_date, input$max_date))
-      }
-
-
-      return(Map(function(x) checkboxGroupInput(NS(id,"pathogen"),"Choose pathogen:",x, selected = x), unique(df$pathogen)))
+      return(shiny::checkboxGroupInput(inputId = NS(id,"pathogen_vars"),
+                                       label = "Choose pathogen:",
+                                       choices = unique(indata$pathogen),
+                                       selected = unique(indata$pathogen))
+             )
 
     })
+
+    output$strat_choices <- shiny::renderUI({
+
+      return(shiny::selectInput(inputId = NS(id,"strat_vars"),
+                                label = "Parameters to stratify by:",
+                                choices = c("None","All",names(indata)), # needs robustness!!
+                                selected = NULL,
+                                multiple = TRUE)
+             )
+    })
+
+    # reacting based on decisions done in ui
+    # from 'pathogen_vars' and 'strat_vars'
+    # shiny::eventReactive(eventExpr = input$pathogen_vars, {
+    #   path_vars <<- input$pathogen_vars
+    #   indata <<- indata %>% filter(pathogen %in% input$pathogen_vars)
+    # })
+
+    shiny::observeEvent(input$pathogen_vars, {
+      tmp <- values$df_data %>% filter(pathogen %in% input$pathogen_vars)
+      values$df_data <- tmp
+    })
+
+    # shiny::eventReactive(eventExpr = input$strat_vars, {
+    #   strat_vars <- input$strat_vars
+    #   if ("None" %in% input$strat_vars) {
+    #     strat_vars <<- c()
+    #   }
+    #
+    #   # 'None' takes precendence to 'All'
+    #   else if ("All" %in% input$strat_vars) {
+    #     strat_vars <<- names(indata)
+    #   }
+    # })
+
   })
+
 }
