74d57586bab321ed86b07a61c031a14c04932c3b
jaemol <jmoller72@gmail.com>
2023-11-16 10:54:39 +0100

    trying to create reactive / reactiveValue to handle data

--- R/mod_tabpanel_data.R
+++ R/mod_tabpanel_data.R
@@ -6,11 +6,11 @@ mod_tabpanel_data_ui <- function(id) {
   shiny::tabPanel(
     "Data",
     # From runExample("09_upload")
-    h2("Load Data"),
-    sidebarLayout(
-      sidebarPanel(
+    shiny::h2("Load Data"),
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
         # Input: Select a file ----
-        fileInput(NS(id,"file1"), "Choose CSV File",
+        shiny::fileInput(NS(id,"file1"), "Choose CSV File",
                   multiple = TRUE,
                   accept = c("text/csv",
                              "text/comma-separated-values,text/plain",
@@ -20,17 +20,17 @@ mod_tabpanel_data_ui <- function(id) {
         tags$hr(),
 
         # Input: Checkbox if file has header ----
-        checkboxInput(NS(id,"header"), "Header", TRUE),
+        shiny::checkboxInput(NS(id,"header"), "Header", TRUE),
 
         # Input: Select separator ----
-        radioButtons(NS(id,"sep"), "Separator",
+        shiny::radioButtons(NS(id,"sep"), "Separator",
                      choices = c(Comma = ",",
                                  Semicolon = ";",
                                  Tab = "\t"),
                      selected = ","),
 
         # Input: Select quotes ----
-        radioButtons(NS(id,"quote"), "Quote",
+        shiny::radioButtons(NS(id,"quote"), "Quote",
                      choices = c(None = "",
                                  "Double Quote" = '"',
                                  "Single Quote" = "'"),
@@ -40,7 +40,7 @@ mod_tabpanel_data_ui <- function(id) {
         tags$hr(),
 
         # Input: Select number of rows to display ----
-        radioButtons(NS(id,"disp"), "Display",
+        shiny::radioButtons(NS(id,"disp"), "Display",
                      choices = c(Head = "head",
                                  All = "all"),
                      selected = "head"),
@@ -49,19 +49,19 @@ mod_tabpanel_data_ui <- function(id) {
         tags$hr(),
 
         # Input: Specify dates?
-        checkboxInput(NS(id,"dates_bin"), "Limit date interval"),
+        shiny::checkboxInput(NS(id,"dates_bin"), "Limit date interval"),
 
         # Input: Select minimum date
-        dateInput(NS(id,"min_date"),"Minimum date:", value = "2023-01-01"),
+        shiny::dateInput(NS(id,"min_date"),"Minimum date:", value = "2023-01-01"),
 
         # Input: Select maximum date
-        dateInput(NS(id,"max_date"),"Maximum date:"),
+        shiny::dateInput(NS(id,"max_date"),"Maximum date:"),
 
         # Horizontal line ----
         tags$hr(),
 
         # Input: Sort data according to date_report
-        checkboxInput(NS(id,"sort_bin"), "Sort data according to date")
+        shiny::checkboxInput(NS(id,"sort_bin"), "Sort data according to date")
 
       ),
 
@@ -69,7 +69,7 @@ mod_tabpanel_data_ui <- function(id) {
       mainPanel(
 
           # Output: Data file ----
-          tableOutput(NS(id,"contents"))
+          shiny::tableOutput(NS(id,"contents"))
 
       )
     ),
@@ -82,9 +82,10 @@ mod_tabpanel_data_server <- function(id) {
   shiny::moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    # Load data - from runExample("09_upload")
-    output$contents <- renderTable({
+    # Load data
+    values <- shiny::reactiveValues(df_data = NULL)
 
+    shiny::observe({
       # input$file1 will be NULL initially. After the user selects
       # and uploads a file, head of that data file by default,
       # or all rows if selected, will be shown.
@@ -93,27 +94,50 @@ mod_tabpanel_data_server <- function(id) {
 
       print(input$file1)
 
-      df <- read.csv(input$file1$datapath,
-                     header = input$header,
-                     sep = input$sep,
-                     quote = input$quote)
+      file_type <- tools::file_ext(input$file1$name)
 
-      if (input$dates_bin) {
-        df <- subset(df, dplyr::between(as.Date(date_report), input$min_date, input$max_date))
+      if (tolower(file_type) == "csv") {
+        # OBS: need to include possibility to read Excel (and .R?)
+        indata <- read.csv(input$file1$datapath,
+                            header = input$header,
+                            sep = input$sep,
+                            quote = input$quote)
       }
 
+      ## NOTE - example, to be removed! ###
+      indata$pathogen[1] <- "E. coli"
+      indata$pathogen[2] <- "Campylobacter"
+      #####################################
+
+      # preprocess
+      indata <- indata %>% dplyr::mutate(date_onset = ifelse(is.na(date_onset) | date_onset=="", date_report, date_onset))
+      indata$age_group <- factor(indata$age_group, levels = stringr::str_sort(unique(indata$age_group), numeric = TRUE))
+      indata$sex <- factor(indata$sex)
+
+      if (input$dates_bin) {
+        indata <- subset(indata, dplyr::between(as.Date(date_report), input$min_date, input$max_date))
+      }
+
+      values$df_data <- indata
+
+    })
+
+    output$contents <- shiny::renderTable({
+      tmp_data <- values$df_data
+
       if (input$sort_bin) {
-        df <- df %>% dplyr::arrange(date_report)
+        tmp_data <- tmp_data %>% dplyr::arrange(date_report)
       }
 
       if(input$disp == "head") {
-        return(head(df, 10))
+        head(tmp_data, 10)
       }
       else {
-        return(df)
+        tmp_data
       }
-
     })
   })
+  return(values)
+
 }
 
