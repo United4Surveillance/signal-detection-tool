cf639885c50c6cea1acc0b327e16fe202ebffb22
Kasper Schou Telkamp <kasper.telkamp@gmail.com>
2023-11-27 13:20:03 +0100

    Changed the aeddo function to conform with PR #65

--- R/aeddo.R
+++ R/aeddo.R
@@ -4,8 +4,8 @@
 #' surveillance data, aggregates it based on specified date variables, and applies
 #' the aeddo algorithm to detect potential outbreaks.
 #'
-#' @param surveillance_data The surveillance data containing information about cases.
-#' @param date_start A date object or character of format yyyy-mm-dd
+#' @param data_aggregated data.frame, aggregated data with case counts
+#' @param number_of_weeks integer, specifying the number of weeks to generate alarms for
 #' @param date_end A date object or character of format yyyy-mm-dd
 #' @param date_var A character specifying the date variable name used for the aggregation. Default is "date_report".
 #' @param population_size The population size for the aeddo algorithm. Default is 1.
@@ -20,10 +20,8 @@
 #'
 #' @references
 #' For information on the aeddo algorithm, refer to the package documentation.
-get_signals_aeddo <- function(surveillance_data,
-                              date_start = NULL,
-                              date_end = NULL,
-                              date_var = "date_report",
+get_signals_aeddo <- function(data_aggregated,
+                              number_of_weeks = 52,
                               population_size = 1,
                               formula = y ~ 1,
                               sig_level = 0.95,
@@ -34,26 +32,13 @@ get_signals_aeddo <- function(surveillance_data,
                               upper = c(Inf, 1e2),
                               method = "L-BFGS-B") {
 
-  checkmate::assert(
-    checkmate::check_null(date_start),
-    checkmate::check_date(lubridate::date(date_start)),
-    combine = "or"
-  )
-  checkmate::assert(
-    checkmate::check_null(date_end),
-    checkmate::check_date(lubridate::date(date_end)),
-    combine = "or"
-  )
-  checkmate::assert(
-    checkmate::check_character(date_var, len = 1, pattern = "date")
-  )
 
-  data <- preprocess_data(surveillance_data) %>%
-    aggregate_data(date_var) %>%
-    add_rows_missing_dates(date_start, date_end)
+  checkmate::assert(
+    checkmate::check_integerish(number_of_weeks)
+  )
 
   # Append the 'time' and population size, 'n', for the 'aeddo' algorithm
-  data <- data %>%
+  data_aggregated <- data_aggregated %>%
     dplyr::mutate(week = formatC(week, width = 2, flag = 0)) %>%
     dplyr::mutate(time = ISOweek::ISOweek2date(paste0(year, "-W", week, "-7")),
                   n = population_size) %>%
@@ -81,11 +66,11 @@ get_signals_aeddo <- function(surveillance_data,
   ranef <- c(pad, aeddo_results$u)
   expected <- c(pad, aeddo_results$lambda)
 
-  data$alarms <- alarms
-  data$upperbound <- upperbound
-  data$ranef <- ranef
-  data$expected <- expected
+  data_aggregated$alarms <- alarms
+  data_aggregated$upperbound <- upperbound
+  data_aggregated$ranef <- ranef
+  data_aggregated$expected <- expected
 
-  return(data)
+  return(data_aggregated)
 
 }
