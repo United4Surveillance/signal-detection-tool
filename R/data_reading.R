e6bc39a8d97b1dfad7d946f7f508aa17be72e7ea
Ann Christin Vietor <VietorA@rki.de>
2024-01-05 17:10:34 +0100

    adding documentation to data_reading functions

--- R/data_reading.R
+++ R/data_reading.R
@@ -1,7 +1,9 @@
-read_csv_both_sep <- function(filepath){
-
-  header <- readLines(filepath, n=1)
-  if(grepl(";",header)){
+#' Read csv files which can have seperators ; or ,
+#' @param filepath character, filepath
+#' @returns data which was read ins
+read_csv_both_sep <- function(filepath) {
+  header <- readLines(filepath, n = 1)
+  if (grepl(";", header)) {
     data <- read.csv(filepath, header = TRUE, sep = ";", encoding = "UTF-8", stringsAsFactors = F)
   } else {
     data <- read.csv(filepath, header = TRUE, sep = ",", encoding = "UTF-8", stringsAsFactors = F)
@@ -10,16 +12,17 @@ read_csv_both_sep <- function(filepath){
   data
 }
 
-read_csv_or_excel <- function(filename, filepath){
-
+#' Read csv or excel files
+#' Checks the input file for its type and then reads the file
+#' @param filename character, filename
+#' @param filepath character, filepath
+read_csv_or_excel <- function(filename, filepath) {
   filetype <- tools::file_ext(filename)
 
-  if(filetype == "csv"){
+  if (filetype == "csv") {
     data <- read_csv_both_sep(filepath)
-  }
-  else if(filetype == "xlsx"| filetype == "xls"){
+  } else if (filetype == "xlsx" | filetype == "xls") {
     data <- readxl::read_excel(filepath, guess_max = 5000)
   }
   data
-
 }
