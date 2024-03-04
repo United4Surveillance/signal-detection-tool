a3598885cadb53754e45ab83f725657874f5e766
jaemol <jaem@ssi.dk>
2024-03-04 14:04:35 +0100

    dividing cols to test- and training data moving from plotly to ggplotly

--- R/plot_time_series.R
+++ R/plot_time_series.R
@@ -19,58 +19,101 @@ plot_time_series <- function(results, interactive = FALSE,
 
   results <- results %>%
     dplyr::ungroup() %>%
-    dplyr::mutate(date = ISOweek::ISOweek2date(
-      paste(.data$year, "-W",
-            stringr::str_pad(.data$week, width = 2, pad = "0"),
-            "-1", sep = ""))) %>%
+    dplyr::mutate(
+      date = ISOweek::ISOweek2date(
+        paste(.data$year, "-W",
+              stringr::str_pad(.data$week, width = 2, pad = "0"),
+              "-1", sep = "")),
+      set_status = dplyr::if_else(is.na(alarms), "Training data", "Test data")) %>%
     dplyr::arrange(date) %>%
     dplyr::slice_tail(n = number_of_weeks)
 
-  col.observed <- "#9E9E9E"
   col.threshold <- "#2297E6"
-  col.expected <- "#000000"
-  col.alarm <- "#DF536B"
+  col.expected  <- "#000000"
+  col.alarm     <- "#DF536B"
+  col.training  <- "#9E9E9E"
+  col.test      <- "#304794"
 
-  # Time-series plot
-  if(interactive) {
+  plot_text <- paste0(
+    results$set_status,
+    "<br>Date: ", results$date, "<br>Observed: ", results$cases,
+    "<br>Threshold: ", round(results$upperbound_pad)
+  )
 
-    p <- plotly::plot_ly(data = results) %>%
-      plotly::add_bars(x = ~date,
-                       y = ~cases,
-                       color = I(col.observed),
-                       hovertemplate = ~ paste0("Date: ", date, "\nObserved: ", .data$cases, "<extra></extra>"),
-                       name = "Observed",
-                       legendgroup = "group1") %>%
-      plotly::add_lines(x = ~date,
-                        y = ~upperbound,
-                        color = I(col.threshold),
-                        line = list(shape = "hvh"),
-                        hovertemplate = ~paste0("Date: ", date, "\nThreshold: ", round(.data$upperbound), "<extra></extra>"),
-                        name = "Threshold",
-                        legendgroup = "group2") %>%
-      plotly::add_lines(x = ~date,
-                        y = ~expected,
-                        color = I(col.expected),
-                        line = list(shape = "hvh"),
-                        hovertemplate = ~paste0("Date: ", date, "\nExpected: ", round(.data$expected), "<extra></extra>"),
-                        name = "Expected",
-                        legendgroup = "group3") %>%
-      plotly::add_markers(data = dplyr::filter(results, .data$alarms == TRUE),
-                          x = ~date,
-                          y = ~cases,
-                          color = I(col.alarm),
-                          marker = list(size = 12),
-                          symbol = I("asterisk-open"),
-                          hovertemplate = ~paste0("Date: ", date, "\nAlarm", "<extra></extra>"),
-                          name = "Alarm", legendgroup = "group4") %>%
-      plotly::layout(xaxis = list(tickformat = "%Y-%m-%d",
-                                  tickvals = pretty(results$date, n = 8),
-                                  title = "Time"),
-                     yaxis = list(title = "Number of Infected",
-                                  tickvals = pretty(c(0, results$cases,
-                                                      results$expected,
-                                                      results$upperbound), 10)),
-                     legend = list(orientation = "h", x = 0.25, y = -0.2)) %>%
+  # if any expected values not NA, then `farrington` is method chosen
+  if (any(!is.na(results$expected_pad))) {
+    plot_text <- paste0(plot_text,
+                        "<br>Expected: ", round(results$expected_pad))
+  }
+
+
+  plt <-
+    results %>%
+    ggplot2::ggplot(ggplot2::aes(x = date, group = 1, text = paste0(
+      set_status,
+      "<br>Date: ", date, "<br>Observed: ", cases,
+      "<br>Threshold: ", round(upperbound),
+      "<br>Expected: ", round(expected)
+    ))) +
+    ggplot2::geom_col(ggplot2::aes(y = cases, fill = set_status)) +
+    ggplot2::geom_step(ggplot2::aes(y = upperbound, color = "Threshold"),
+                       linewidth = 1, direction = "hv") +
+    ggplot2::geom_step(ggplot2::aes(y = upperbound_pad, color = "Threshold", linetype = "Test data"),
+                       linewidth = 0.3, direction = "hv") +
+    ggplot2::geom_point(data = dplyr::filter(results, alarms == TRUE),
+                        ggplot2::aes(y = cases, shape = alarms),
+                        color = col.alarm, size = 4)
+
+  if (any(!is.na(results$expected_pad))) {
+    plt <- plt +
+      ggplot2::geom_step(ggplot2::aes(y = expected, color = "Expected"),
+                         linewidth = 1, direction = "hv") +
+      ggplot2::geom_step(ggplot2::aes(y = expected_pad, color = "Expected", linetype = "Training data"),
+                         linewidth = 0.3, direction = "hv")
+  }
+
+  plt <- plt +
+    ggplot2::scale_x_date(breaks = scales::breaks_pretty(n = 8),
+                          date_labels = "%Y-%d-%m") +
+    ggplot2::scale_y_continuous(breaks = scales::breaks_pretty(n = 10),
+                                expand = ggplot2::expansion(mult = c(0, 0.1))) +
+    ggplot2::scale_shape_manual(values = c("TRUE" = 8),
+                                labels = c("TRUE" = "Alarm")) +
+    ggplot2::scale_color_manual(values = c("Expected" = col.expected,
+                                           "Threshold" = col.threshold)) +
+    ggplot2::scale_fill_manual(values = c("Test data" = col.test, "Training data" = col.training),
+                               labels = c("Test data", "Training data")) +
+    ggplot2::scale_linetype_manual(values = c("Training data" = 1, "Test data" = 1), name = "", guide = "none") +
+    ggplot2::theme(
+      legend.position   = "bottom",
+      legend.background = ggplot2::element_blank(),
+      legend.key  = ggplot2::element_blank(),
+      legend.text = ggplot2::element_text(size = 12),
+
+      panel.background   = ggplot2::element_blank(),
+      panel.grid.major.x = ggplot2::element_blank(),
+      panel.grid.minor.x = ggplot2::element_blank(),
+      panel.grid.major.y = ggplot2::element_line(colour = "grey75"),
+      panel.grid.minor.y = ggplot2::element_blank(),
+
+      axis.line  = ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_blank(),
+      axis.text  = ggplot2::element_text(size = 12),
+      axis.title.x = ggplot2::element_text(face = "bold", size = 14),
+      axis.title.y = ggplot2::element_text(face = "bold", size = 14)) +
+    ggplot2::guides(fill  = ggplot2::guide_legend(order = 1),
+                    color = ggplot2::guide_legend(order = 2),
+                    shape = ggplot2::guide_legend(order = 3)) +
+    ggplot2::labs(x = "Time",
+                  y = "Number of infected",
+                  color = NULL,
+                  fill  = NULL,
+                  shape = NULL)
+
+  if (interactive) {
+    plt <- plotly::ggplotly(plt, tooltip = "text") %>%
+      plotly::layout(legend = list(orientation = "h", x = 0.3, y = -0.1)) %>%
+      plotly::style(hoverlabel = list(bgcolor = "white")) %>%
       plotly::config(modeBarButtonsToRemove = c('autoScale2d',
                                                 # 'resetScale2d',
                                                 'select2d',
@@ -81,50 +124,9 @@ plot_time_series <- function(results, interactive = FALSE,
                                                 'zoom2d',
                                                 'toggleSpikelines'))
 
-  } else{
-
-    p <- results %>%
-      ggplot2::ggplot(ggplot2::aes(x = date)) +
-      ggplot2::geom_col(ggplot2::aes(y = .data$cases, fill = "Observed")) +
-      ggplot2::geom_step(ggplot2::aes(y = .data$upperbound, color = "Threshold"),
-                         linewidth = 1, direction = "mid") +
-      ggplot2::geom_step(ggplot2::aes(y = .data$expected, color = "Expected"),
-                         linewidth = 1, direction = "mid") +
-      ggplot2::geom_point(data = dplyr::filter(results, .data$alarms == TRUE),
-                          ggplot2::aes(y = .data$cases, shape = .data$alarms),
-                          color = col.alarm, size = 4) +
-      ggplot2::scale_x_date(breaks = scales::breaks_pretty(n = 8),
-                            date_labels = "%Y-%d-%m") +
-      ggplot2::scale_y_continuous(breaks = scales::breaks_pretty(n = 10),
-                         expand = ggplot2::expansion(mult = c(0, 0.1))) +
-      ggplot2::scale_shape_manual(values = c("TRUE" = 8),
-                                  labels = c("TRUE" = "Alarm")) +
-      ggplot2::scale_color_manual(values = c("Expected" = col.expected,
-                                             "Threshold" = col.threshold)) +
-      ggplot2::scale_fill_manual(values = c("Observed" = col.observed)) +
-      ggplot2::theme(
-        legend.position = "bottom",
-        panel.background = ggplot2::element_blank(),
-        axis.line = ggplot2::element_blank(),
-        axis.ticks = ggplot2::element_blank(),
-        legend.background = ggplot2::element_blank(),
-        legend.key = ggplot2::element_blank(),
-        panel.grid.major.x = ggplot2::element_blank(),
-        panel.grid.minor.x = ggplot2::element_blank(),
-        panel.grid.major.y = ggplot2::element_line(colour = "grey75"),
-        panel.grid.minor.y = ggplot2::element_blank(),
-        axis.title.x = ggplot2::element_text(face = "bold"),
-        axis.title.y = ggplot2::element_text(face = "bold")) +
-      ggplot2::guides(fill = ggplot2::guide_legend(order = 1),
-                      color = ggplot2::guide_legend(order = 2),
-                      shape = ggplot2::guide_legend(order = 3)) +
-      ggplot2::labs(x = "Time",
-                    y = "Number of infected",
-                    color = NULL,
-                    fill = NULL,
-                    shape = NULL)
   }
 
-  p
+  return(plt)
+
 
 }
