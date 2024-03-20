9073ccca72c6d34ee8332a2e2022255f82bb9472
Ann Christin Vietor <VietorA@rki.de>
2024-03-20 15:57:31 +0100

    - remove ungroup() statement at beginning as results entering the function should always be ungrouped before - code automatic formating

--- R/plot_time_series.R
+++ R/plot_time_series.R
@@ -1,4 +1,3 @@
-
 #' Plot time-series based on the results of Farrington Flexible
 #'
 #' @param results data returned by the get_signals_farringtonflexible()
@@ -16,24 +15,26 @@
 #' }
 plot_time_series <- function(results, interactive = FALSE,
                              number_of_weeks = 52) {
-
   results <- results %>%
-    dplyr::ungroup() %>%
     dplyr::mutate(
       date = ISOweek::ISOweek2date(
         paste(.data$year, "-W",
-              stringr::str_pad(.data$week, width = 2, pad = "0"),
-              "-1", sep = "")),
+          stringr::str_pad(.data$week, width = 2, pad = "0"),
+          "-1",
+          sep = ""
+        )
+      ),
       set_status = dplyr::if_else(is.na(alarms), "Training data", "Test data"),
-      set_status = factor(set_status, levels = c("Training data", "Test data"))) %>%
+      set_status = factor(set_status, levels = c("Training data", "Test data"))
+    ) %>%
     dplyr::arrange(date) %>%
     dplyr::slice_tail(n = number_of_weeks)
 
   col.threshold <- "#2297E6"
-  col.expected  <- "#000000"
-  col.alarm     <- "#FF0000"
-  col.training  <- "#9E9E9E"
-  col.test      <- "#304794"
+  col.expected <- "#000000"
+  col.alarm <- "#FF0000"
+  col.training <- "#9E9E9E"
+  col.test <- "#304794"
 
 
   plt <-
@@ -42,89 +43,114 @@ plot_time_series <- function(results, interactive = FALSE,
       ifelse(set_status == "Test data", "Signal detection period", ""),
       "<br>Date: ", date,
       "<br>Observed: ", cases,
-      ifelse(!is.na(upperbound_pad) | !is.na(upperbound),(
+      ifelse(!is.na(upperbound_pad) | !is.na(upperbound), (
         ifelse(is.na(upperbound_pad),
-               paste0("<br>Threshold: ", round(upperbound)),
-               paste0("<br>Threshold: ", round(upperbound_pad)))
+          paste0("<br>Threshold: ", round(upperbound)),
+          paste0("<br>Threshold: ", round(upperbound_pad))
+        )
       ), ""),
       ifelse(!is.na(expected_pad) | !is.na(expected), (
         ifelse(is.na(expected_pad),
-               paste0("<br>Expected: ", round(expected)),
-               paste0("<br>Expected: ", round(expected_pad)))
+          paste0("<br>Expected: ", round(expected)),
+          paste0("<br>Expected: ", round(expected_pad))
+        )
       ), "")
     ))) +
     ggplot2::geom_col(ggplot2::aes(y = cases, fill = set_status)) +
     ggplot2::geom_step(ggplot2::aes(y = upperbound, color = "Threshold"),
-                       linewidth = 1.3, direction = "hv") +
+      linewidth = 1.3, direction = "hv"
+    ) +
     ggplot2::geom_step(ggplot2::aes(y = upperbound_pad, color = "Threshold", linetype = "Test data"),
-                       linewidth = 0.3, direction = "hv")
+      linewidth = 0.3, direction = "hv"
+    )
 
   if (any(!is.na(results$expected_pad))) {
     plt <- plt +
       ggplot2::geom_step(ggplot2::aes(y = expected, color = "Expected"),
-                         linewidth = 1.3, direction = "hv") +
+        linewidth = 1.3, direction = "hv"
+      ) +
       ggplot2::geom_step(ggplot2::aes(y = expected_pad, color = "Expected", linetype = "Training data"),
-                         linewidth = 0.3, direction = "hv")
+        linewidth = 0.3, direction = "hv"
+      )
   }
 
   # adding alarm points
   plt <- plt +
-    ggplot2::geom_point(data = dplyr::filter(results, alarms == TRUE),
-                        ggplot2::aes(y = cases, shape = alarms, stroke = 1),
-                        color = col.alarm, size = 6)
+    ggplot2::geom_point(
+      data = dplyr::filter(results, alarms == TRUE),
+      ggplot2::aes(y = cases, shape = alarms, stroke = 1),
+      color = col.alarm, size = 6
+    )
 
   plt <- plt +
-    ggplot2::scale_x_date(date_breaks = "1 month", date_labels = "%Y-%m-%d",
-                          expand = c(0,0)) +
-    ggplot2::scale_y_continuous(breaks = scales::breaks_pretty(n = 10),
-                                expand = ggplot2::expansion(mult = c(0, 0.1))) +
-    ggplot2::scale_shape_manual(values = c("TRUE" = 8),
-                                labels = c("TRUE" = "Alarm")) +
-    ggplot2::scale_color_manual(values = c("Expected" = col.expected,
-                                           "Threshold" = col.threshold)) +
-    ggplot2::scale_fill_manual(values = c("Test data" = col.test, "Training data" = col.training),
-                               labels = c("Test data"="Signal detection period"),
-                               breaks = c("Test data")) +
+    ggplot2::scale_x_date(
+      date_breaks = "1 month", date_labels = "%Y-%m-%d",
+      expand = c(0, 0)
+    ) +
+    ggplot2::scale_y_continuous(
+      breaks = scales::breaks_pretty(n = 10),
+      expand = ggplot2::expansion(mult = c(0, 0.1))
+    ) +
+    ggplot2::scale_shape_manual(
+      values = c("TRUE" = 8),
+      labels = c("TRUE" = "Alarm")
+    ) +
+    ggplot2::scale_color_manual(values = c(
+      "Expected" = col.expected,
+      "Threshold" = col.threshold
+    )) +
+    ggplot2::scale_fill_manual(
+      values = c("Test data" = col.test, "Training data" = col.training),
+      labels = c("Test data" = "Signal detection period"),
+      breaks = c("Test data")
+    ) +
     ggplot2::scale_linetype_manual(values = c("Training data" = 1, "Test data" = 1), name = "", guide = "none") +
     ggplot2::theme(
-      legend.position   = "top",
+      legend.position = "top",
       legend.background = ggplot2::element_blank(),
-      legend.key  = ggplot2::element_blank(),
+      legend.key = ggplot2::element_blank(),
       legend.text = ggplot2::element_text(size = 12),
-
-      panel.background   = ggplot2::element_blank(),
+      panel.background = ggplot2::element_blank(),
       panel.grid.major.x = ggplot2::element_blank(),
       panel.grid.minor.x = ggplot2::element_blank(),
       panel.grid.major.y = ggplot2::element_line(colour = "grey75"),
       panel.grid.minor.y = ggplot2::element_blank(),
-
-      axis.line  = ggplot2::element_blank(),
+      axis.line = ggplot2::element_blank(),
       axis.ticks.length.x = ggplot2::unit(0.25, "cm"),
-      axis.text  = ggplot2::element_text(size = 12),
+      axis.text = ggplot2::element_text(size = 12),
       axis.text.x = ggplot2::element_text(angle = 15, hjust = 1, vjust = 1),
       axis.title.x = ggplot2::element_text(face = "bold", size = 14),
-      axis.title.y = ggplot2::element_text(face = "bold", size = 14)) +
-    ggplot2::guides(fill  = ggplot2::guide_legend(order = 1),
-                    color = ggplot2::guide_legend(order = 2),
-                    shape = ggplot2::guide_legend(order = 3)) +
-    ggplot2::labs(x = "Time",
-                  y = "Number of infected",
-                  color = NULL,
-                  fill  = NULL,
-                  shape = NULL)
+      axis.title.y = ggplot2::element_text(face = "bold", size = 14)
+    ) +
+    ggplot2::guides(
+      fill = ggplot2::guide_legend(order = 1),
+      color = ggplot2::guide_legend(order = 2),
+      shape = ggplot2::guide_legend(order = 3)
+    ) +
+    ggplot2::labs(
+      x = "Time",
+      y = "Number of infected",
+      color = NULL,
+      fill = NULL,
+      shape = NULL
+    )
 
   if (interactive) {
     plt <- plotly::ggplotly(plt, tooltip = "text") %>%
-      plotly::layout(legend = list(orientation = "h", x = 0.5, y = -0.9,
-                                   yanchor = "bottom", xanchor = "center")) %>%
-      plotly::config(modeBarButtonsToRemove = c('autoScale2d',
-                                                'select2d',
-                                                'lasso2d',
-                                                'zoomIn2d',
-                                                'zoomOut2d',
-                                                'pan2d',
-                                                'zoom2d',
-                                                'toggleSpikelines'))
+      plotly::layout(legend = list(
+        orientation = "h", x = 0.5, y = -0.9,
+        yanchor = "bottom", xanchor = "center"
+      )) %>%
+      plotly::config(modeBarButtonsToRemove = c(
+        "autoScale2d",
+        "select2d",
+        "lasso2d",
+        "zoomIn2d",
+        "zoomOut2d",
+        "pan2d",
+        "zoom2d",
+        "toggleSpikelines"
+      ))
 
     # modifying the interactive plot legend
     plt$x$data[[1]]$showlegend <- FALSE
@@ -142,6 +168,4 @@ plot_time_series <- function(results, interactive = FALSE,
   }
 
   return(plt)
-
-
 }
