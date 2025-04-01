abaa378a9b75c6499f78536dab68aa1f993f1849
tinneuro <tin.vietor@t-online.de>
2025-04-01 16:14:10 +0200

    adding a type column to the metadata and updating that _id variables can be both numeric or character and date variables both character or date

--- data-raw/input_metadata.R
+++ data-raw/input_metadata.R
@@ -1,6 +1,6 @@
 ## code to prepare `input_metadata` dataset goes here
-input_metadata <- read.delim("dev/data/input/description.md", sep = "|") %>%
-  dplyr::select("Variable", "Mandatory", "Description") %>%
+input_metadata <- read.delim("dev/data/input/description.md", header = TRUE, sep = "|") %>%
+  dplyr::select("Variable", "Mandatory","Type", "Description") %>%
   dplyr::mutate_all(trimws) %>%
   dplyr::filter(!grepl("-{1,}", Variable))
 
