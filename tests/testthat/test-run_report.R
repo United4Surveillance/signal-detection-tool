c748193875ebd9618944855c3df055a0ab7943e8
Estupinan Mendez, Juan <Estupinan-MendezJ@rki.de>
2025-09-25 11:00:52 +0200

    run_report test checks for zip file instead of html

--- tests/testthat/test-run_report.R
+++ tests/testthat/test-run_report.R
@@ -1,66 +1,66 @@
 test_that("run_report() successfully produces an HTML", {
   # check basics by using fast and easy algorithm EARS
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".zip")
   on.exit(unlink(output_path), add = TRUE)
   output_file <- basename(output_path)
   output_dir <- dirname(output_path)
 
   run_report(data = input_example, report_format = "HTML", method = "EARS", strata = "age_group", output_file = output_file, output_dir = output_dir)
 
-  expect_true(file.exists(paste0(output_path, ".html")))
+  expect_true(file.exists(output_path))
 })
 
 test_that("run_report() successfully produces a docx", {
   # check basics by using fast and easy algorithm EARS
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".docx")
   on.exit(unlink(output_path), add = TRUE)
   output_file <- basename(output_path)
   output_dir <- dirname(output_path)
 
   run_report(data = input_example, report_format = "DOCX", method = "EARS", strata = "age_group", output_file = output_file, output_dir = output_dir)
 
-  expect_true(file.exists(paste0(output_path, ".docx")))
+  expect_true(file.exists(output_path))
 })
 
 
 test_that("run_report() successfully works without any strata selected", {
   # check basics by using fast and easy algorithm EARS
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".zip")
   on.exit(unlink(output_path), add = TRUE)
   output_file <- basename(output_path)
   output_dir <- dirname(output_path)
 
   run_report(data = input_example, report_format = "HTML", method = "EARS", strata = NULL, output_file = output_file, output_dir = output_dir)
 
-  expect_true(file.exists(paste0(output_path, ".html")))
+  expect_true(file.exists(output_path))
 })
 
 test_that("run_report() successfully works for a selected intervention_date", {
   # check basics by using fast and easy algorithm EARS
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".zip")
   on.exit(unlink(output_path), add = TRUE)
   output_file <- basename(output_path)
   output_dir <- dirname(output_path)
 
   run_report(data = input_example, report_format = "HTML", method = "Mean", strata = NULL, output_file = output_file, output_dir = output_dir, intervention_date = "2021-03-01")
 
-  expect_true(file.exists(paste0(output_path, ".html")))
+  expect_true(file.exists(output_path))
 })
 
 test_that("run_report() successfully works for a different number of weeks", {
   # check basics by using fast and easy algorithm EARS
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".zip")
   on.exit(unlink(output_path), add = TRUE)
   output_file <- basename(output_path)
   output_dir <- dirname(output_path)
 
   run_report(data = input_example, report_format = "HTML", method = "Mean", strata = NULL, output_file = output_file, output_dir = output_dir, number_of_weeks = 12)
 
-  expect_true(file.exists(paste0(output_path, ".html")))
+  expect_true(file.exists(output_path))
 })
 
 test_that("run_report() works with pre-computed signals_agg and signals_pad", {
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".zip")
   on.exit(unlink(output_path), add = TRUE)
 
   disease <- unique(input_example$pathogen)
@@ -91,12 +91,12 @@ test_that("run_report() works with pre-computed signals_agg and signals_pad", {
     signals_pad = signals_agg_pad$signals_pad %>% dplyr::mutate(pathogen = disease)
   )
 
-  expect_true(file.exists(paste0(output_path, ".html")))
+  expect_true(file.exists(output_path))
 })
 
 
 test_that("run_report() works with a custom bslib theme", {
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".zip")
   on.exit(unlink(output_path), add = TRUE)
 
   theme <- bslib::bs_theme(primary = "firebrick", bootswatch = "minty")
@@ -111,11 +111,11 @@ test_that("run_report() works with a custom bslib theme", {
     custom_theme = theme
   )
 
-  expect_true(file.exists(paste0(output_path, ".html")))
+  expect_true(file.exists(output_path))
 })
 
 test_that("run_report() works with multiple pathogens", {
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".zip")
   on.exit(unlink(output_path), add = TRUE)
 
   run_report(
@@ -127,11 +127,11 @@ test_that("run_report() works with multiple pathogens", {
     output_dir = dirname(output_path)
   )
 
-  expect_true(file.exists(paste0(output_path, ".html")))
+  expect_true(file.exists(output_path))
 })
 
 test_that("run_report() works with a subset of pathogens from a line list with multiple pathogens", {
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".zip")
   on.exit(unlink(output_path), add = TRUE)
 
   selected_pathogens <- unique(input_example_multipathogen$pathogen)[1:2]
@@ -145,11 +145,11 @@ test_that("run_report() works with a subset of pathogens from a line list with m
     output_dir = dirname(output_path)
   )
 
-  expect_true(file.exists(paste0(output_path, ".html")))
+  expect_true(file.exists(output_path))
 })
 
 test_that("run_report() works with multiple pathogens using precomputed signals_agg and signals_padded", {
-  output_path <- tempfile()
+  output_path <- tempfile(fileext = ".zip")
   on.exit(unlink(paste0(output_path, ".html")), add = TRUE)
 
   # Select two pathogens
@@ -197,5 +197,5 @@ test_that("run_report() works with multiple pathogens using precomputed signals_
     signals_pad = signals_padded
   )
 
-  expect_true(file.exists(paste0(output_path, ".html")))
+  expect_true(file.exists(output_path))
 })
