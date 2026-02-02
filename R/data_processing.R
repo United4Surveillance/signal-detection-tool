165f4bbb3c34810d90402e093b34adf1e460b160
jeremiasweb <WeberJ@rki.de>
2026-02-02 12:50:33 +0100

    now using a reset and sex_levels() to adress the issue

--- R/data_processing.R
+++ R/data_processing.R
@@ -90,8 +90,10 @@ preprocess_data <- function(data) {
 
   # sex is not mandatory
   if ("sex" %in% colnames(data)) {
+    # reset sex_levels
+    app_cache_env$sex_levels <- c("male", "female", "diverse", NA_character_)
     data <- data %>%
-      dplyr::mutate(sex = factor(sex, levels = sex_raw_levels()))
+      dplyr::mutate(sex = factor(sex, levels = sex_levels()))
   }
 
   data
