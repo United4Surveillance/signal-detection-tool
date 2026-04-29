5e06ff3cb406f35cd7be9a6d9e3847ae8b54c603
Estupinan Mendez, Juan <Estupinan-MendezJ@rki.de>
2026-04-29 11:39:53 +0200

    add condition for EARS and CUSUM to return NA for strength score
    Scaling factor for strength score

--- R/score_strength.R
+++ R/score_strength.R
@@ -1,30 +1,37 @@
 #' @title Score signals according to size strength
 #' @description Scores each signal in the signals_res object using the signal size,
 #' defined by the distance from expected value and upperbound threshold value
-#' the score follows the formula (1-size)/size
+#' the score follows the formula (size^3-1)/size^3. For EARS and CUSOUM method, the 
+#' function returns a score of NA
 #'
-#' @param signals_res
+#' @param signals_res dataframe with the results of signal detection
 #'
 #' @returns strength scores for signals
 #' @export
 score_strength <- function(signals_res){
 
   method <- unique(signals_res$method)
-  possible_algorithms <- available_algorithms()[!names(available_algorithms()) %in% c("EARS", "CUSUM")]
 
-  checkmate::assert_choice(method, possible_algorithms)
+  if(method %in% c("EARS", "CUSUM")){
+    # strength score not possible with EARS or CUSUM as they don't return an expected value
+    signals_res <- signals_res %>% 
+      dplyr::mutate(
+        size.alarm = NA,
+        score = NA
+      )
+  } else {
+    signals_res <- signals_res %>%
+      dplyr::mutate(size.alarm = (.data$cases-.data$expected)/(.data$upperbound-.data$expected))
 
-  signals_res <- signals_res %>%
-    dplyr::mutate(size.alarm = (.data$cases-.data$expected)/(.data$upperbound-.data$expected))
-
-  signals_res <- signals_res %>%
+    signals_res <- signals_res %>%
     dplyr::mutate(
       score = dplyr::case_when(
         is.na(.data$alarms) ~ NA,
-        !alarms ~ NA,
-        .default = (.data$size.alarm - 1)/.data$size.alarm
+        isFALSE(.data$alarms) ~ NA,
+        .default = ((.data$size.alarm^3) - 1)/(.data$size.alarm^3)
       )
     )
+  }
 
   return(signals_res %>% dplyr::select(c(".row_id", "score")))
 }
