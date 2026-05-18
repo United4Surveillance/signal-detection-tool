f8f2ff741c297ae242fc9c26b70e9b198f881243
Jere Weber <WeberJ@rki.de>
2026-05-18 17:12:55 +0200

    improved run_report()-check

--- R/run_report.R
+++ R/run_report.R
@@ -257,8 +257,11 @@ run_report <- function(
   checkmate::assert(
     checkmate::check_string(title, null.ok = TRUE)
   )
+
   if (!grepl("cusum", method, ignore.case = TRUE) && !grepl("ears", method, ignore.case = TRUE)
-      && !grepl("farringtonflexible", method, ignore.case = TRUE)) {
+      && !grepl("farringtonflexible", method, ignore.case = TRUE) &&
+      "outbreak_status" %in% names(data)
+    ) {
     checkmate::assert(
       checkmate::check_true(filter_outbreak_cases),
       checkmate::check_false(filter_outbreak_cases),
