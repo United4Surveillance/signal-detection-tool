449bc7c57998a25fbb287bade4efcae5e22228e6
Jere Weber <WeberJ@rki.de>
2026-05-19 17:09:42 +0200

    fix test period description failure

--- R/mod_tabpanel_input.R
+++ R/mod_tabpanel_input.R
@@ -229,11 +229,18 @@ mod_tabpanel_input_server <- function(id, data, errors_detected) {
       shiny::req(input$n_weeks)
       shiny::req(iv_weeks$is_valid())
 
+      # Use extension date if available, otherwise latest date in filtered data
+      max_date_dataset <- if (!is.null(date_ext())) {
+        date_ext()
+      } else {
+        max(filtered_data()$date_report, na.rm = TRUE)
+      }
+
       # subtracting 1 from input$n_weeks to get correct dates for flooring (issue #256)
-      date_floor <- lubridate::floor_date(max(filtered_data()$date_report) - lubridate::weeks(input$n_weeks - 1),
+      date_floor <- lubridate::floor_date(max_date_dataset - lubridate::weeks(input$n_weeks - 1),
         week_start = 1, unit = "week"
       )
-      date_ceil <- lubridate::ceiling_date(max(filtered_data()$date_report), unit = "week", week_start = 7)
+      date_ceil <- lubridate::ceiling_date(max_date_dataset, unit = "week", week_start = 7)
       paste("Chosen signal detection period from", date_floor, "to", date_ceil)
     })
 
