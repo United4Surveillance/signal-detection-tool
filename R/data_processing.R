0ddc097a14fbb835ae28f81c3bf17715445f2298
Jere Weber <WeberJ@rki.de>
2026-05-27 13:11:24 +0200

    warn when outbreak case exclusion cannot be applied in aggregation

--- R/data_processing.R
+++ R/data_processing.R
@@ -197,6 +197,14 @@ aggregate_data <- function(data,
   if (!("outbreak_status" %in% names(data))) {
     data_agg <- data_agg %>%
       dplyr::select(-cases_in_outbreak)
+
+    if (exclude_outbreak_cases_from_fitting){
+      warning(
+        "No exclusion of outbreak-associated case counts was performed despite ",
+        "exclude_outbreak_cases_from_fitting = TRUE, ",
+        "because `outbreak_status` is not present in the data."
+      )
+      }
   }
 
   data_agg %>%
