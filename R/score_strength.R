5a88837990751dfd9b0bf142416e3a15292f4f35
Estupinan Mendez, Juan <Estupinan-MendezJ@rki.de>
2026-07-14 15:44:22 +0200

    rounding for score seasonal and score strength to allow closed [0,1] range

--- R/score_strength.R
+++ R/score_strength.R
@@ -1,8 +1,8 @@
 #' @title Score signals according to size strength
 #' @description Scores each signal in the signals_res object using the signal size,
 #' defined by the distance from expected value and upperbound threshold value
-#' the score follows the formula (size^3-1)/size^3. For EARS and CUSUM method, the
-#' function returns a score of NA
+#' the score follows the formula (size^3-1)/size^3 and then is rounded to the 2nd decimal point.
+#' For EARS and CUSUM method, the function returns a score of NA.
 #'
 #' @param signals_res dataframe with the results of signal detection
 #'
@@ -39,7 +39,7 @@ score_strength <- function(signals_res) {
     signals_res <- signals_res %>%
       dplyr::mutate(
         score = dplyr::case_when(
-          .data$alarms ~ ((.data$size.alarm^3) - 1) / (.data$size.alarm^3),
+          .data$alarms ~ round(((.data$size.alarm^3) - 1) / (.data$size.alarm^3), digits = 2),
           .default = NA
         )
       )
