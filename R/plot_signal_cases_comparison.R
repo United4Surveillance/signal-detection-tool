8c2c0cb3b54fcd1d55b62448af7f0dfdd23da97f
Jere Weber <WeberJ@rki.de>
2026-08-18 17:03:22 +0200

    fix checkmate induced “No signals found”-error

--- R/plot_signal_cases_comparison.R
+++ R/plot_signal_cases_comparison.R
@@ -21,6 +21,7 @@ plot_agegroup_comparison <- function(data_agg, number_of_time_units, time_unit) 
     min.cols = 2
   )
 
+  if (any(data_agg$signal, na.rm = TRUE)) {
   checkmate::assert_integerish(
     number_of_time_units,
     lower = 1,
@@ -32,6 +33,7 @@ plot_agegroup_comparison <- function(data_agg, number_of_time_units, time_unit) 
     choices = c("weekly", "biweekly", "monthly"),
     null.ok = FALSE
   )
+  }
 
   # Use time unit labels
   time_unit_label <- dplyr::case_when(
