9b5de91bb5d692c9a4523e5570d544bfb4ebe78d
jeremiasweb <jeremiasweb@users.noreply.github.com>
2026-08-18 15:10:26 +0000

    Style code (GHA)

--- R/plot_signal_cases_comparison.R
+++ R/plot_signal_cases_comparison.R
@@ -22,17 +22,17 @@ plot_agegroup_comparison <- function(data_agg, number_of_time_units, time_unit) 
   )
 
   if (any(data_agg$signal, na.rm = TRUE)) {
-  checkmate::assert_integerish(
-    number_of_time_units,
-    lower = 1,
-    len = 1
-  )
+    checkmate::assert_integerish(
+      number_of_time_units,
+      lower = 1,
+      len = 1
+    )
 
-  checkmate::assert_choice(
-    time_unit,
-    choices = c("weekly", "biweekly", "monthly"),
-    null.ok = FALSE
-  )
+    checkmate::assert_choice(
+      time_unit,
+      choices = c("weekly", "biweekly", "monthly"),
+      null.ok = FALSE
+    )
   }
 
   # Use time unit labels
